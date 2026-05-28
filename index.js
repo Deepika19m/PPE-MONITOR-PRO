6926d8013065fd89cac9e971efff062a0e8d40c5
Deepika M <153338780+Deepika19m@users.noreply.github.com>
2026-05-28 12:49:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+new Promise((resolveOuter) => {
+  resolveOuter(
+    new Promise((resolveInner) => {
+      setTimeout(resolveInner, 1000);
+    }),
+  );
+});
+
+  const myPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("foo");
+  }, 300);
+});
+
+myPromise
+  .then(handleFulfilledA, handleRejectedA)
+  .then(handleFulfilledB, handleRejectedB)
+  .then(handleFulfilledC, handleRejectedC);
